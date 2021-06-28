95362291188ee2a10b13c53a337c492a5c6f75fd
dhhhruv <84224538+dhhhruv@users.noreply.github.com>
2021-06-28 15:34:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links')
+
+
+const navslide = () => {
+
+    const burger
+    const nav = 
+}
+
+burger.addEventListener('click', () => {
+
+    nav.classList.toggle('nav-active');
+})
+
+
+navslide();
