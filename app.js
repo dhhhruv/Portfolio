e766e0f5f5d3a8d312fe1592addd032f7c2c68b6
dhhhruv <84224538+dhhhruv@users.noreply.github.com>
2021-07-11 16:36:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.nav-links')
+// const burger = document.querySelector('.burger');
+// const nav = document.querySelector('.nav-links')
 
 
-const navslide = () => {
+// const navslide = () => {
 
-    const burger
-    const nav = 
-}
+//     const burger
+//     const nav = 
+// }
 
-burger.addEventListener('click', () => {
+// burger.addEventListener('click', () => {
 
-    nav.classList.toggle('nav-active');
+//     nav.classList.toggle('nav-active');
+// })
+
+
+// navslide();
+
+
+
+const resume = document.querySelector('.resume');
+
+
+resume.addEventListener('click', () => {
+
+    alert("The resume will be uploaded soon...!!");
 })
-
-
-navslide();
